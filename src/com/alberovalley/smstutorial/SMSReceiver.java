f9805478dca8816c49c0001cabf85e7d2ba0aec6
frank@alberovalley <alberovalley@gmail.com>
2012-07-16 11:50:50 +0200

    recepción de sms lista

--- src/com/alberovalley/smstutorial/SMSReceiver.java
+++ src/com/alberovalley/smstutorial/SMSReceiver.java
@@ -3,12 +3,34 @@ package com.alberovalley.smstutorial;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 public class SMSReceiver extends BroadcastReceiver {
 
 	@Override
-	public void onReceive(Context arg0, Intent arg1) {
+	public void onReceive(Context context, Intent intent) {
+		Bundle bundle = intent.getExtras();
+		// obtenemos el array de estructuras pdus
+		Object messages[] = (Object[]) bundle.get("pdus");
 
+		SmsMessage smsMessage[] = new SmsMessage[messages.length];
+
+		for (int n = 0; n < messages.length; n++) {
+			// generamos mensajes sms a partir de las estructuras pdu			
+			smsMessage[n] = SmsMessage.createFromPdu((byte[]) messages[n]);
+		}
+
+		// mostramos el primer mensaje
+		Toast toast = Toast.makeText(context, "SMS Recibido: " + 
+				smsMessage[0].getMessageBody(), Toast.LENGTH_LONG);
+		toast.show();
+		/* algunas de las propiedades que pueden obtenerse de un SMS 
+		smsMessage[0].getDisplayOriginatingAddress() //dirección de origen
+		smsMessage[0].getIndexOnSim() // posición en la SIM
+		smsMessage[0].getStatusOnSim() //estado: leído, sin leer, envíado, sin enviar
+		*/
 	}
 
 }
